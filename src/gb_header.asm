14d882fddd6d0421418713b23632a7d783ef098a
Scott LaVigne <1406859+lavignes@users.noreply.github.com>
2024-03-31 23:21:58 -0700

    Update gb_header.asm

--- src/gb_header.asm
+++ src/gb_header.asm
@@ -3,8 +3,9 @@
 
 ?include "hardware.inc"
 
-jp Start
-nop
+EntryPoint:
+    di
+    jp Start
 
 HW_ROM_HEADER HW_ROM_HEADER_COMPAT_CGB_ONLY, HW_ROM_HEADER_MBC_RAM_BAT,    \
               HW_ROM_HEADER_ROM_SIZE_4096KB, HW_ROM_HEADER_SRAM_SIZE_128KB
